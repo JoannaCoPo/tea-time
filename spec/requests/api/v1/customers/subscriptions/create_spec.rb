998a31567a6d26b5ce0c1325e1d50df4b8cd9dd9
JoannaCoPo <jococopo@gmail.com>
2021-11-02 13:51:54 -0600

    Adjust block naming

--- spec/requests/api/v1/customers/subscriptions/create_spec.rb
+++ spec/requests/api/v1/customers/subscriptions/create_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'user creation request' do
+describe 'request to create a user subscription' do
   describe 'happy path' do
     it 'creates a new tea subscription for a customer' do
       tea = create(:tea)
@@ -23,7 +23,7 @@ describe 'user creation request' do
       expect(subscription[:data][:type]).to eq('subscription')
       expect(data[:title]).to eq(tea.name)
       expect(data[:price]).to eq(24.99)
-      expect(data[:status]).to eq("active") 
+      expect(data[:status]).to eq("active")
       expect(data[:frequency]).to eq('biannually')
       expect(data[:customer_id]).to eq(customer.id)
       expect(data[:tea_id]).to eq(tea.id)
