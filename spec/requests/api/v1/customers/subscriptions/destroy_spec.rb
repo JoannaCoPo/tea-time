3b7935e0e012ded87a29a5c0063924bfc6469a08
JoannaCoPo <jococopo@gmail.com>
2021-11-02 13:52:23 -0600

    Test delete requuest for a customer subscription

--- spec/requests/api/v1/customers/subscriptions/destroy_spec.rb
+++ spec/requests/api/v1/customers/subscriptions/destroy_spec.rb
@@ -6,11 +6,12 @@ describe 'request to cancel a user subscription' do
       customer = create(:customer)
       subscription1 = create(:subscription, customer_id: customer.id)
       subscription2 = create(:subscription, customer_id: customer.id)
-
+require "pry"; binding.pry
       delete "/api/v1/customers/#{customer.id}/subscriptions/#{subscription2.id}"
 
       expect(response).to be_successful
       expect(Subscription.find_by(id: subscription1.id)).to eq(subscription1)
+      require "pry"; binding.pry
       expect(Subscription.find_by(id: subscription2.id)).to eq(nil)
     end
   end
