236c9445850a4de22d29dfecffad8e79cd138da7
JoannaCoPo <jococopo@gmail.com>
2021-11-04 22:17:40 -0600

    Test error handling

--- spec/requests/api/v1/customers/subscriptions/create_spec.rb
+++ spec/requests/api/v1/customers/subscriptions/create_spec.rb
@@ -47,9 +47,21 @@ describe 'request to create a user subscription' do
       expect(response.status).to eq(400)
       expect(subscription[:errors]).to eq("Validation failed: Price can't be blank")
     end
+
+    it 'returns an error if a tea does not exist' do
+      customer = create(:customer)
+      body = { "tea_id": 100,
+                "title": 'Nonexistent Tea',
+                "frequency": 2
+              }
+      headers = { 'Content-Type': 'application/json' }
+
+      post "/api/v1/customers/#{customer.id}/subscriptions", headers: headers, params: body.to_json
+      subscription = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+      expect(subscription[:errors]).to eq("Tea variety not found")
+    end
   end
 end
-
-# Add coverage for:
-# returns error if tea does not exsit
-# returns error if customer tries to create a duplicate subscription
